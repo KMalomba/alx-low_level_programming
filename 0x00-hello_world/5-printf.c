201982386a35723aa6540e7d5f97fe625fa756ab
KMalomba <kevinmalomba014@gmail.com>
2021-06-26 04:53:40 -0700

    use of printf to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - The entry point of my program
+ *
+ *Return: Return 0 if there is no error in main program
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+	return (0);
+}
